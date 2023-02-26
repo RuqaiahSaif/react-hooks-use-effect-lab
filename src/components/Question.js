ea097410e010778d9e59b4770711f82c9690e422
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2023-02-26 18:27:13 +0300

    Completed assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,34 @@
-import React, { useState } from "react";
+import { set } from "lodash";
+import { timeout } from "q";
+import React, { useEffect, useState } from "react";
+
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+      return;
+    }
+
+
+    const timeout = setTimeout(() => {
+      // decrement the time remaining
+      setTimeRemaining((timeRemaining) => timeRemaining - 1);
+    }, 1000);
+
+    // clean up by using clearInterval() method 
+    return function () {
+      clearTimeout(timeout);
+    };
+  }, [timeRemaining, onAnswered]);
+
+
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
